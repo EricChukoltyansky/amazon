6a1f50a8828ef6ec0485e5a87b45c460bcf61864
EricChukoltyansky <kazohelmet@gmail.com>
2021-11-26 13:35:25 +0200

    Cart js done

---
+++ script/index.js
@@ -0,0 +1,14 @@
+let btnAdd = document.querySelector(".btn-function-cart-add");
+let btnDec = document.querySelector(".btn-function-cart-dec");
+let zero = document.querySelector(".zero");
+
+const increment = () => {
+  zero.textContent++;
+};
+const decrement = () => {
+  +zero.textContent === 0 ? (zero.textContent = 0) : zero.textContent--;
+};
+
+btnAdd.addEventListener("click", increment);
+
+btnDec.addEventListener("click", decrement);
