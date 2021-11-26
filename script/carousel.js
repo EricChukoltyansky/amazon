0e14e149d6929436613000e20f1e6e90917d5da9
EricChukoltyansky <kazohelmet@gmail.com>
2021-11-26 20:10:46 +0200

    Finished js

--- script/carousel.js
+++ script/carousel.js
@@ -11,15 +11,14 @@ let cards = [
   },
   {
     id: "2",
-    img: "/assets/img/41egj-AierL._AC_SR160,200_ (1)",
-    content:
-      "Costume Wig | Qaccf 80s Wigs Halloween Costumes Male Rock Wig Long Culry Punk Heavy..",
+    img: "/assets/img/89e6ab0d-91d0-47ab-a0e7-477b4fed6b08.tmp",
+    content: "Costume Wig | Qaccf 80s Wigs Halloween Costumes Male Rock Wig",
     stars: "img-stars-four-halves",
     price: "$18.99",
   },
   {
     id: "3",
-    img: "/assets/img/41-LONvRxVL._AC_SR160,200_ (1)",
+    img: "/assets/img/eaf9cfb0-da5d-4532-a681-fc19c7c0bf41.tmp",
     content:
       "Dress Up American Gold Ninja Costume - Fierce Samurai Warrior Costume for Boys and..",
     stars: "img-stars-four-halves",
@@ -27,7 +26,7 @@ let cards = [
   },
   {
     id: "4",
-    img: "/assets/img/51EEpkqm2KL._AC_SR160,200_ (1)",
+    img: "/assets/img/ea7158af-cef2-4790-b458-f869f1ed550c.tmp",
     content:
       "Ambesonne Japanese Apron, Vintage Style Oriental Themed Demon Samurai Eastern Style..",
     stars: "img-stars-four",
@@ -35,7 +34,7 @@ let cards = [
   },
   {
     id: "5",
-    img: "/assets/img/41lChV9A-xL._AC_SR160,200_ (1)",
+    img: "/assets/img/da704f3b-5a4e-40e5-9499-0d32ce3a28c4.tmp",
     content:
       "Topcosplay Mens Hair Wigs Black Short Curly Fluffy Cosplay Halloween Costume..",
     stars: "img-stars-four",
@@ -43,7 +42,7 @@ let cards = [
   },
   {
     id: "6",
-    img: "/assets/img/31sxiNhAD9L._AC_SR160,200_ (1)",
+    img: "/assets/img/d256ef52-be9a-440d-b2e0-b6b218e060ec.tmp",
     content:
       "STfantasy Men Short Black Wigs Male Guy Layered Wavy Halloween Cosplay Anime Party..",
     stars: "img-stars-three-halves",
@@ -51,7 +50,7 @@ let cards = [
   },
   {
     id: "7",
-    img: "/assets/img/51lMO2dWUjL._AC_SR160,200_ (1)",
+    img: "/assets/img/aa9962d5-618b-4f7d-981f-9d4eb0d55ace.tmp",
     content:
       "Funny Party Hats Rainbow Troll Wig - Colorful 80s Wigs - Neon Spiked Crazy Costume..",
     stars: "img-stars-four-halves",
@@ -59,7 +58,7 @@ let cards = [
   },
   {
     id: "8",
-    img: "/assets/img/418oltGVYfL._AC_SR160,200_ (1)",
+    img: "/assets/img/a35aca7c-68f3-47d1-8304-6aa0407d15b9.tmp",
     content:
       "THY COLLECTIBLES Women's Silk Traditional Japanese Kimono Robe/Bathrobe/Party..",
     stars: "img-stars-four-halves",
@@ -67,7 +66,7 @@ let cards = [
   },
   {
     id: "9",
-    img: "/assets/img/67bd8171-bb2d-4aae-bdb7-2d6b037d04cb.tmp",
+    img: "/assets/img/d9a97fb4-3019-4ad3-b54e-543d4d6c4f9d.tmp",
     content:
       "Seidarise Men's Haori Jacket Kimono Cardigan Yukata Noragi Happi Coat",
     stars: "img-stars-four-halves",
@@ -75,7 +74,7 @@ let cards = [
   },
   {
     id: "10",
-    img: "/assets/img/313ZIsyH16L._AC_SR160,200_ (1)",
+    img: "/assets/img/41d0432c-fd15-4abe-aeef-04f5cfbb5376.tmp",
     content:
       "STfantasy Mens Brown Wig Wavy Mid Length Synthetic Hair for Male Cosplay Winter Soldier..",
     stars: "img-stars-three-halves",
@@ -92,7 +91,7 @@ let cards = [
   {
     id: "12",
     img: "/assets/img/eaf9cfb0-da5d-4532-a681-fc19c7c0bf41.tmp",
-    content: "Edoten Men's Japan Kimono Denim Samue NV M",
+    content: "Edoten Men's Japan Kimono Denim Samue NV M Edoten Men's Japan Kimono Denim Samue NV M",
     stars: "img-stars-three-halves",
     price: "$89.90",
   },
@@ -106,7 +105,7 @@ let cards = [
   },
   {
     id: "14",
-    img: "/assets/img/a35aca7c-68f3-47d1-8304-6aa0407d15b9.tmp",
+    img: "/assets/img/58bc836a-336e-4108-babe-dcd73c7f8c1e.tmp",
     content:
       "Hitman Black Wig 90s Mens Costume Wigs Black Natural Hairstyle Can Tie into Ponytail..",
     stars: "img-stars-three-halves",
@@ -122,9 +121,9 @@ let cards = [
   },
   {
     id: "16",
-    img: "/assets/img/da704f3b-5a4e-40e5-9499-0d32ce3a28c4.tmp",
+    img: "/assets/img/ec3470a4-7f90-4eb0-904d-c17d80cfc8be.tmp",
     content:
-      "AKStore Wigs 32' 80cm Long Straight Anime Fashion Women's Cosplay Wig With Free..",
+      "Creamily Men’s Black Synthetic Short Wigs Layered Full Replacement Hair..",
     stars: "img-stars-three-halves",
     price: "$15.85",
   },
@@ -135,25 +134,45 @@ let rightBtn = document.querySelector(".carousel-right-btn");
 let items = document.querySelectorAll(".carousel-img-container");
 
 let changeCards = () => {
-  cards.forEach((card, index) => {
-    console.log(
-      `<img
+  items.forEach((item, index) => {
+    item.innerHTML = `<img
+                    class="object-img"
+                    src="${cards[index + 8].img}"
+                    alt=""
+                  />
+                  <a href=""
+                    ><p class="object-content">
+                      ${cards[index + 8].content}
+                    </p></a
+                  >
+                  <div class="img-stars ${cards[index + 8].stars}"></div>
+                  <div class="prime">
+                    <span class="object-price">${cards[index + 8].price}</span>
+                    <div class="prime-sticker"></div>
+                  </div>`;
+  });
+};
+
+let loadCards = () => {
+  items.forEach((item, index) => {
+    item.innerHTML = `<img
                   class="object-img"
-                  src="${card[index].img}"
+                  src="${cards[index].img}"
                   alt=""
                 />
                 <a href=""
                   ><p class="object-content">
-                    ${card[index].content}
+                    ${cards[index].content}
                   </p></a
                 >
-                <div class="img-stars">${card[index].stars}</div>
+                <div class="img-stars ${cards[index].stars}"></div>
                 <div class="prime">
-                  <span class="object-price">${card[index].price}</span>
+                  <span class="object-price">${cards[index].price}</span>
                   <div class="prime-sticker"></div>
-                </div>`
-    );
+                </div>`;
   });
 };
 
 rightBtn.addEventListener("click", changeCards);
+leftBtn.addEventListener("click", loadCards);
+window.addEventListener("load", loadCards);
