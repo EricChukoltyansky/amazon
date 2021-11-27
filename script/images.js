e745f0743ed5eab6cbbeb6f28b1d238df9e06cd5
EricChukoltyansky <kazohelmet@gmail.com>
2021-11-27 12:21:20 +0200

    Responsivity

--- script/images.js
+++ script/images.js
@@ -1,9 +1,35 @@
-let div1 = document.querySelector(".div1");
-let div2 = document.querySelector(".div2");
-let div3 = document.querySelector(".div3");
+// let div1 = document.querySelector(".div1");
+// let div2 = document.querySelector(".div2");
+// let div3 = document.querySelector(".div3");
 
+// div1.addEventListener("mouseover", () => {
+//   let largeImage1 = document.querySelector(".wig-image1").style.display;
+//   let largeImage2 = document.querySelector(".wig-image2").style.display;
+//   let largeImage3 = document.querySelector(".wig-image3").style.display;
+//   largeImage1 = "block";
+//   largeImage2 = "none";
+//   largeImage3 = "none";
+//   console.log(largeImage1, largeImage2, largeImage3);
+// });
 
+// div2.addEventListener("mouseover", () => {
+//   let largeImage1 = document.querySelector(".wig-image1").style.display;
+//   let largeImage2 = document.querySelector(".wig-image2").style.display;
+//   let largeImage3 = document.querySelector(".wig-image3").style.display;
+//   largeImage1 = "none";
+//   largeImage2 = "block";
+//   largeImage3 = "none";
+//   console.log(largeImage1, largeImage2, largeImage3);
+// });
 
+// div3.addEventListener("mouseover", () => {
+//   let largeImage1 = document.querySelector(".wig-image1").style.display;
+//   let largeImage2 = document.querySelector(".wig-image2").style.display;
+//   let largeImage3 = document.querySelector(".wig-image3").style.display;
+//   largeImage1 = "none";
+//   largeImage2 = "none";
+//   largeImage3 = "block";
+//   console.log(largeImage1, largeImage2, largeImage3);
+// });
 
-
-div1.addEventListener('mouseover', switchImages1);
+// console.log("hello");
